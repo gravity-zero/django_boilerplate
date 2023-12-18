21354917c2590b7f08ba485fc610192329a54f43
gravity-zero <gravity.neo@gmail.com>
2023-12-18 15:54:13 +0100

    update localhost server

--- init.sh
+++ init.sh
@@ -6,5 +6,5 @@ django-admin startproject blog
 if [ -d ./blog ]; then
   cd ./blog
   python manage.py migrate
-  python manage.py runserver 127.0.0.1:8000
+  python manage.py runserver 0.0.0.0:8000
 fi
