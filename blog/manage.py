015eca7e448cb631fe95a120ed9cd4fb37f86ad3
gravity-zero <gravity.neo@gmail.com>
2023-12-19 10:18:50 +0100

    add mysql connexion + dotenv

--- blog/manage.py
+++ blog/manage.py
@@ -2,7 +2,20 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.db import connections
+from dotenv import dotenv_values
 
+config = dotenv_values(".env.dev")  # config = {"USER": "foo", "EMAIL": "foo@example.org"}
+
+print("URL du projet: "+ config['ROOT_URL'])
+# Vérifier la connexion à MySQL
+try:
+    connection = connections['default']
+    cursor = connection.cursor()
+    cursor.execute("SELECT 1")
+    print("Connexion à MySQL réussie !")
+except Exception as e:
+    print(f"Erreur de connexion à MySQL : {e}")
 
 def main():
     """Run administrative tasks."""
